bd830ab35522f5d4539f0774b47e68a81d85259b
Kevanana26 <525kpiccinich@frhsd.com>
2024-03-22 10:43:05 -0400

    updated js (ow my head)

--- script.js
+++ script.js
@@ -10,8 +10,27 @@ function addName() {
     displayNames()// Call the  displayNames function to update the list
 
     nameInput.value=' ' // clear the input field after adding the name
-    if (!name.trim()) {
-        alert('Buddy, please enter an actual question that can be answered')
+    if (name === null) {
+        document.getElementById('response-text').innerText = 'Enater a name, please and thank you'
+        document.getElementById('response-text').classList = 'display text-danger'
+        document.getElementById('response-image').src = '#BADIMAGE' //fix this shit later//
         return
     }
 }
+function displayNames() {
+    const nameList = document.getElementById('nameList')
+    nameList.innerHTML = '' //clear out the previous list items
+
+    for (let i = 0; i < namesAraay.length; i++){
+        const name = namesArray[i]
+
+        const li =document.createElement('li')
+        li.className='list-group-item'
+        const span = document.createElement('span')
+        span.textContent = name
+
+        li.appendChild(span)
+        nameList.appendChild(li)
+    }
+}
+document.getElementById('addNameBtn').addEventListener('click', addName)
